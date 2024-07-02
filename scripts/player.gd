06363da2d2b551253e6a96675cc4e5802fed24d2
aryuserious <aryusure@outlook.com>
2024-07-02 17:14:01 -0400

    basic player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,1 +1,16 @@
+class_name Player
+
 extends CharacterBody2D
+
+
+
+var speed = 300
+
+func _physics_process(_delta):
+    move()
+
+
+func move():
+    var dir = Input.get_vector("left", "right", "up", "down")
+    velocity = dir * speed
+    move_and_slide()
