3c1979a83f2dda835822cb2062c0365334ae7bf5
aryuserious <aryusure@outlook.com>
2024-07-02 21:19:47 -0400

    clean up ingredient code also added a peppermint scene to the Level 1 scene

--- scripts/ingredient.gd
+++ scripts/ingredient.gd
@@ -7,7 +7,7 @@ enum type { GINGER_ROOT, PEPPERMINT_CANDY, THORNED_ROSE, HONEY, LEECH, SALT, CAT
 
 
 func _ready():
+    # RANDOM LOCATION #
     var x = randi_range(0, 1152)
     var y = randi_range(0, 648)
-
     position = Vector2(x, y)
