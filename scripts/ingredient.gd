6788da0654a8bb7b41ae8c3209e74e5531b74b5c
aryuserious <aryusure@outlook.com>
2024-07-02 20:02:16 -0400

    ingredient goes to random position within viewport onready

--- scripts/ingredient.gd
+++ scripts/ingredient.gd
@@ -1,3 +1,13 @@
-extends Node
-
 class_name Ingredient
+
+extends Area2D
+
+
+# members
+
+
+func _ready():
+    var x = randi_range(0, 1152)
+    var y = randi_range(0, 648)
+
+    position = Vector2(x, y)
