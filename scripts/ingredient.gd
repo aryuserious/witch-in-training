b9e955694265162f13c0412d6c873006875831e2
aryuserious <aryusure@outlook.com>
2024-07-04 04:49:09 -0400

    ingredients set their own texture - all ingredient textures are in one exported PNG - the set their region based off their ingredient_type - the ingredients that have textures so far are: - ginger root, peppermint candy, thorned_rose, and salt

--- scripts/ingredient.gd
+++ scripts/ingredient.gd
@@ -4,10 +4,25 @@ extends Area2D
 
 enum type { GINGER_ROOT, PEPPERMINT_CANDY, THORNED_ROSE, HONEY, LEECH, SALT, CAT_HAIR }
 @export var ingredient_type : type
+var ingredient_regions = {
+    "ginger root" : Rect2(123, 15, 55, 57),
+    "peppermint candy" : Rect2(4, 16, 63, 59),
+}
 
 
 func _ready():
+    set_sprite_texture()
     # RANDOM LOCATION #
     var x = randi_range(0, 960)
     var y = randi_range(0, 540)
     position = Vector2(x, y)
+
+
+func set_sprite_texture():
+    match ingredient_type:
+        type.GINGER_ROOT:
+            if has_node("Sprite"):
+                $Sprite.region_rect = ingredient_regions["ginger root"]
+        type.PEPPERMINT_CANDY:
+            if has_node("Sprite"):
+                $Sprite.region_rect = ingredient_regions["peppermint candy"]
