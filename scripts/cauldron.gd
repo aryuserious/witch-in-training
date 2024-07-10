1a0b50f66b534a505f1eba608c1394b9d800e2db
aryuserious <aryusure@outlook.com>
2024-07-10 00:35:25 -0400

    Cauldron class has potion inner class

--- scripts/cauldron.gd
+++ scripts/cauldron.gd
@@ -3,10 +3,13 @@ class_name Cauldron
 extends StaticBody2D
 
 
-var ingredients_needed : Array[Ingredient]
 var current_ingredients : Array[Ingredient]
 var player_in_range : bool = false
 var player : Player
+
+var potions = {
+    "Healing Potion" : "potion.new() here"
+}
 
 
 func _physics_process(_delta):
@@ -22,3 +25,22 @@ func _on_detection_body_entered(body:Node2D):
 func _on_detection_body_exited(body:Node2D):
     if body is Player:
         player_in_range = false
+
+
+class Potion:
+    enum rarities {
+        COMMON = 0,
+        UNCOMMON = 1,
+        SPECIAL = 2,
+        RARE = 3,
+        EXTRA_RARE = 4,
+        LEGENDARY = 5
+    }
+    var rarity : rarities
+    var ingredients : Array[Ingredient]
+    var effect = ""
+
+    func _init(ingredients_param : Array[Ingredient], effect_param : String, rarity_param = rarities.COMMON):
+        ingredients = ingredients_param
+        effect = effect_param
+        rarity = rarity_param
