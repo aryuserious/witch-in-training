9a0416c36df008b95343f2cf00f173fe6c8e90b7
aryuserious <aryusure@outlook.com>
2024-08-07 16:20:18 -0400

    global autoload checks for high score

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,3 +1,11 @@
 extends Node
 
 var difficulty : int = 0
+var new_high_score : bool
+var high_score : int = 0
+var score : int = 0
+
+
+func _process(_delta):
+    if score > high_score:
+        high_score = score
