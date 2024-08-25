93b7164b79900cb02a025e3162e58bc1fa48aca8
aryuserious <aryusure@outlook.com>
2024-08-25 14:51:34 -0400

    game difficulty increases with score

--- scripts/global.gd
+++ scripts/global.gd
@@ -9,3 +9,19 @@ var score : int = 0
 func _process(_delta):
     if score > high_score:
         high_score = score
+    
+    match score:
+        0:
+            difficulty = 0
+        5:
+            difficulty = 1
+        10:
+            difficulty =  2
+        15:
+            difficulty = 3
+        20:
+            difficulty = 4
+        25:
+            difficulty = 5
+        _:
+            difficulty = difficulty
