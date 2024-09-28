e8ca6c7c0ea60e37c2fc2b02828af53add875e76
aryuserious <aryusure@outlook.com>
2024-09-28 00:08:27 -0400

    Pixel art for each ingredient is in the game! -28th day!

--- scripts/ingredient.gd
+++ scripts/ingredient.gd
@@ -4,22 +4,25 @@ extends Area2D
 
 enum type { GINGER_ROOT, PEPPERMINT_CANDY, THORNED_ROSE, HONEY, LEECH, SALT, CAT_HAIR }
 @export var ingredient_type : type
-var ingredient_regions = {
-	"ginger root" : Rect2(123, 15, 55, 57),
-	"peppermint candy" : Rect2(4, 16, 63, 59),
-	"thorned rose" : Rect2(224, 21, 51, 49),
-	"salt" : Rect2(308, 21, 67, 55)
+var ingredient_regions = { # the number that is the key matches with the value of each enum element
+	0: Rect2(16, 0, 8, 8),
+	1: Rect2(8, 0, 8, 8),
+	2: Rect2(24, 0, 8, 8),
+	3: Rect2(40, 0, 8, 8),
+	4: Rect2(48, 0, 8, 8),
+	5: Rect2(0, 0, 8, 8),
+	6: Rect2(32, 0, 8, 8)
 }
 
 
 func _ready():
 	# random location
-	var x = randi_range(0, 320)
-	var y = randi_range(0, 180)
+	var x = randi_range(100, 500)
+	var y = randi_range(30, 400)
 	global_position = Vector2(x, y)
 
 	# set sprite based off type
-	set_sprite_region()
+	set_sprite_region(ingredient_type)
 	#print("ingredient made")
 
 
@@ -29,20 +32,21 @@ static func create(ingr_type : type) -> Ingredient:
 	return ingredient
 
 
-func set_sprite_region():
-	match ingredient_type:
-		type.GINGER_ROOT:
-			if has_node("Sprite"):
-				$Sprite.region_rect = ingredient_regions["ginger root"]
-		type.PEPPERMINT_CANDY:
-			if has_node("Sprite"):
-				$Sprite.region_rect = ingredient_regions["peppermint candy"]
-		type.THORNED_ROSE:
-			if has_node("Sprite"):
-				$Sprite.region_rect = ingredient_regions["thorned rose"]
-		type.SALT:
-			if has_node("Sprite"):
-				$Sprite.region_rect = ingredient_regions["salt"]
+func set_sprite_region(ingr_type):
+	$Sprite.region_rect = ingredient_regions[ingr_type]
+	#match ingredient_type:
+		#type.GINGER_ROOT:
+			#if has_node("Sprite"):
+				#$Sprite.region_rect = ingredient_regions["ginger root"]
+		#type.PEPPERMINT_CANDY:
+			#if has_node("Sprite"):
+				#$Sprite.region_rect = ingredient_regions["peppermint candy"]
+		#type.THORNED_ROSE:
+			#if has_node("Sprite"):
+				#$Sprite.region_rect = ingredient_regions["thorned rose"]
+		#type.SALT:
+			#if has_node("Sprite"):
+				#$Sprite.region_rect = ingredient_regions["salt"]
 
 
 static func ingredient_type_as_string(ingr_type : Ingredient.type) -> String:
