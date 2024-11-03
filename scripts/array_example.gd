c937587079b7bcba03c0eee0a4806bfdbc1d5b8e
aryuserious <aryusure@outlook.com>
2024-11-03 11:19:34 -0500

    clean up files, rename some things

--- scripts/array_example.gd
+++ scripts/array_example.gd
@@ -5,14 +5,14 @@ var b = a
 var c = []
 
 func _ready():
-    for item in a:
-        c.append(item)
+	for item in a:
+		c.append(item)
 
-    c.append("HI")
-    print(a) # prints [3, 4, 8]
+	c.append("HI")
+	print(a) # prints [3, 4, 8]
 
-    b.append("GROUND")
-    print(a) # prints [3, 4, 8, "GROUND"]
+	b.append("GROUND")
+	print(a) # prints [3, 4, 8, "GROUND"]
 
-    # Lesson: when setting b to a, if we edit b, we also edit a. when individually adding each element to c,
-    # and we edit c, a stays the same, which is what i want in this case.
+	# Lesson: when setting b to a, if we edit b, we also edit a. when individually adding each element to c,
+	# and we edit c, a stays the same, which is what i want in this case.
